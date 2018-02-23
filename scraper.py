033620bb5b0e085e575bcabe25d2eef47d224c9c
SergioLaRosa <36663933+SergioLaRosa@users.noreply.github.com>
2018-02-23 14:12:42 +0100

    Fixed trend_processing output error

--- scraper.py
+++ scraper.py
@@ -3,104 +3,121 @@ import datetime
 import sys
 import csv
 import re
-import time, datetime
+import time
+import datetime
 try:
-	import text_sentiment as sc
+    import text_sentiment as sc
 except ImportError:
-	print("[ERROR] Unable to import text_sentiment module: can't run!")
-	sys.exit()
+    print("[ERROR] Unable to import text_sentiment module: can't run!")
+    sys.exit()
 try:
-	from textblob import TextBlob
+    from textblob import TextBlob
 except ImportError:
-	print("[ERROR] Unable to import TextBlob module: can't run!")
-	sys.exit()
+    print("[ERROR] Unable to import TextBlob module: can't run!")
+    sys.exit()
 try:
-	import tweepy
+    import tweepy
 except ImportError:
-	print("[ERROR] Unable to import Tweepy module: can't run!")
-	sys.exit()
+    print("[ERROR] Unable to import Tweepy module: can't run!")
+    sys.exit()
+
 
 class Scraper():
 
-	def __init__(self):
-		self._api = ""
-	
-	def get_tweets(self, api, data, num, targets):
-		self._api = api
-		self._data = data
-		self._num = int(num)
-		self._targets = targets
-		self._tweets = []
+    def __init__(self):
+        self._api = ""
 
-		self._analyzer = sc.TweetSentiment()
+    def get_tweets(self, api, data, num, targets):
+        self._api = api
+        self._data = data
+        self._num = int(num)
+        self._targets = targets
+        self._tweets = []
 
-		try:
-			if os.name == "nt":
-				self._csv_tweet = open(self._data, 'w', encoding='utf-8', newline='')
-			else:
-				self._csv_tweet = open(self._data, 'w')
-			self._csv_tweet_writer = csv.writer(self._csv_tweet)
-			self._csv_tweet_writer.writerow(["created_at", "location", "user", "full_text"])
+        self._analyzer = sc.TweetSentiment()
 
-			self._parsed_data = ((str(self._data))[:-4])+"_sentiment.csv"
-			self._csv_data = open(self._parsed_data, 'w')
-			self._csv_data_writer = csv.writer(self._csv_data)
-			self._csv_data_writer.writerow(["tweet", "sentiment"])
+        try:
+            if os.name == "nt":
+                self._csv_tweet = open(
+                    self._data, 'w', encoding='utf-8', newline='')
+            else:
+                self._csv_tweet = open(self._data, 'w')
+            self._csv_tweet_writer = csv.writer(self._csv_tweet)
+            self._csv_tweet_writer.writerow(
+                ["created_at", "location", "user", "full_text"])
 
-		except:
-			print("[ERROR] Unable to prepare CSV files!")
-			sys.exit()
+            self._parsed_data = ((str(self._data))[:-4]) + "_sentiment.csv"
+            self._csv_data = open(self._parsed_data, 'w')
+            self._csv_data_writer = csv.writer(self._csv_data)
+            self._csv_data_writer.writerow(["tweet", "sentiment"])
 
-		
-		print("[*] Fetching tweets and processing 'tweets sentiment' data")
-		print("[*] Please, wait...\n")
+        except BaseException:
+            print("[ERROR] Unable to prepare CSV files!")
+            sys.exit()
 
-		try:
-			
-			for self._tweet in tweepy.Cursor(self._api.search, q=self._targets, count=self._num, 
-				lang="en", tweet_mode="extended").items(self._num):
+        print("[*] Fetching tweets and processing 'tweets sentiment' data")
+        print("[*] Please, wait...\n")
 
-				try:
-					self._csv_tweet_writer.writerow([
-						self._tweet.created_at,
-						self._tweet.user.location, 
-						self._tweet.user.screen_name.encode('utf-8'),
-						self._tweet.full_text.encode('ascii', errors='ignore'),
-						])
-				except:
-					print("[ERROR] Unable to write tweets on file: ", self._data)
-					sys.exit()
+        try:
 
-				try:
-					self._parsed_tweet = {}
-					self._parsed_tweet['user'] = self._tweet.user.screen_name.encode('utf-8')
-					self._parsed_tweet['text'] = self._tweet.full_text.encode('ascii', 'ignore')
-					self._parsed_tweet['sentiment'] = self._analyzer.get_tweet_sentiment(self._tweet.full_text)
+            for self._tweet in tweepy.Cursor(
+                    self._api.search,
+                    q=self._targets,
+                    count=self._num,
+                    lang="en",
+                    tweet_mode="extended").items(
+                    self._num):
 
-					if self._tweet.retweet_count > 0:
-						# if tweet has retweets, ensure that it is appended only once
-						if self._parsed_tweet not in self._tweets:
-							self._tweets.append(self._parsed_tweet)
-					else:
-						self._tweets.append(self._parsed_tweet)
+                try:
+                    self._csv_tweet_writer.writerow([
+                        self._tweet.created_at,
+                        self._tweet.user.location,
+                        self._tweet.user.screen_name.encode('utf-8'),
+                        self._tweet.full_text.encode('ascii', errors='ignore'),
+                    ])
+                except BaseException:
+                    print(
+                        "[ERROR] Unable to write tweets on file: ",
+                        self._data)
+                    sys.exit()
 
-					self._csv_data_writer.writerow([
-						self._tweet.full_text.encode('ascii', 'ignore'),
-						self._parsed_tweet['sentiment']
-						])
-				except:
-					print("[ERROR] Unable to write analysis on file: ", self._parsed_data)
-					sys.exit()
+                try:
+                    self._parsed_tweet = {}
+                    self._parsed_tweet['user'] = self._tweet.user.screen_name.encode(
+                        'utf-8')
+                    self._parsed_tweet['text'] = self._tweet.full_text.encode(
+                        'ascii', 'ignore')
+                    self._parsed_tweet['sentiment'] = self._analyzer.get_tweet_sentiment(
+                        self._tweet.full_text)
 
-			print("[SUCCESS] Tweets written on file: ", self._data)
-			print("[SUCCESS] Tweets sentiment analysis written on: ", self._parsed_data)	
+                    if self._tweet.retweet_count > 0:
+                        # if tweet has retweets, ensure that it is appended
+                        # only once
+                        if self._parsed_tweet not in self._tweets:
+                            self._tweets.append(self._parsed_tweet)
+                    else:
+                        self._tweets.append(self._parsed_tweet)
 
-			return self._tweets
+                    self._csv_data_writer.writerow([
+                        self._tweet.full_text.encode('ascii', 'ignore'),
+                        self._parsed_tweet['sentiment']
+                    ])
+                except BaseException:
+                    print(
+                        "[ERROR] Unable to write analysis on file: ",
+                        self._parsed_data)
+                    sys.exit()
 
-		except tweepy.TweepError as e:
-			print("[ERROR: TWEEPY API] " + str(e))
-			sys.exit()
-		except:
-			print("[ERROR] Unable to get/save tweets!")
-			sys.exit()
+            print("[SUCCESS] Tweets written on file: ", self._data)
+            print(
+                "[SUCCESS] Tweets sentiment analysis written on: ",
+                self._parsed_data)
 
+            return self._tweets
+
+        except tweepy.TweepError as e:
+            print("[ERROR: TWEEPY API] " + str(e))
+            sys.exit()
+        except BaseException:
+            print("[ERROR] Unable to get/save tweets!")
+            sys.exit()
--- twitter_tea.py
+++ twitter_tea.py
@@ -5,147 +5,178 @@ import sys
 import argparse
 import csv
 try:
-	import auth_check as ack
+    import auth_check as ack
 except ImportError:
-	print("[ERROR] Unable to import Auth. module: can't run!")
-	sys.exit()
+    print("[ERROR] Unable to import Auth. module: can't run!")
+    sys.exit()
 try:
-	import scraper as scr
+    import scraper as scr
 except ImportError:
-	print("[ERROR] Unable to import Scraper module: can't run!")
-	sys.exit()
+    print("[ERROR] Unable to import Scraper module: can't run!")
+    sys.exit()
 try:
-	import post_processing as pp
+    import post_processing as pp
 except ImportError:
-	print("[ERROR] Unable to import Post processing module: can't run!")
-	sys.exit()
+    print("[ERROR] Unable to import Post processing module: can't run!")
+    sys.exit()
 
 
 def get_args():
-	_parser = argparse.ArgumentParser(description='Script retrieves tweets with one/more hashtags')
-	_parser.add_argument('-a', '--auth', type=str, help='User auth. file', required=True)
-	_parser.add_argument('-o', '--ofile', type=str, help='CSV output filename', required=False, default="tweet_search.csv")
-	_parser.add_argument('-n', '--tnum', type=int, help='Number of tweets to get', required=True, default=500)
-	_parser.add_argument('-t', '--hashtag', type=str, help='Hashtag to search', required=True)
-	_parser.add_argument('-l', '--limitf', type=int, help='Limit hasht. frequency for plotter', required=False, default=50)
+    _parser = argparse.ArgumentParser(
+        description='Script retrieves tweets with one/more hashtags')
+    _parser.add_argument(
+        '-a',
+        '--auth',
+        type=str,
+        help='User auth. file',
+        required=True)
+    _parser.add_argument(
+        '-o',
+        '--ofile',
+        type=str,
+        help='CSV output filename',
+        required=False,
+        default="tweet_search.csv")
+    _parser.add_argument(
+        '-n',
+        '--tnum',
+        type=int,
+        help='Number of tweets to get',
+        required=True,
+        default=500)
+    _parser.add_argument(
+        '-t',
+        '--hashtag',
+        type=str,
+        help='Hashtag to search',
+        required=True)
+    _parser.add_argument(
+        '-l',
+        '--limitf',
+        type=int,
+        help='Limit hasht. frequency for plotter',
+        required=False,
+        default=50)
 
-	_args = _parser.parse_args()
-	_u_auth = _args.auth
-	_outfile = _args.ofile
-	_num = int(_args.tnum)
-	_hashtag = _args.hashtag
-	_limit = int(_args.limitf)
+    _args = _parser.parse_args()
+    _u_auth = _args.auth
+    _outfile = _args.ofile
+    _num = int(_args.tnum)
+    _hashtag = _args.hashtag
+    _limit = int(_args.limitf)
 
-	return _u_auth, _outfile, _num, _hashtag, _limit
+    return _u_auth, _outfile, _num, _hashtag, _limit
 
 
 if __name__ == "__main__":
 
-	try:
+    try:
 
-		_u_auth, _outfile, _num, _hashtag, _limit = get_args()
-		_parsed_data = ((str(_outfile))[:-4])+"_sentiment.csv"
+        _u_auth, _outfile, _num, _hashtag, _limit = get_args()
+        _parsed_data = ((str(_outfile))[:-4]) + "_sentiment.csv"
 
-		print("[INFO] Twitter extraction tool\n")
+        print("[INFO] Twitter extraction tool\n")
 
-		_auth = ack.Authenticator()
-		_auth = _auth.auth_setup(_u_auth)
+        _auth = ack.Authenticator()
+        _auth = _auth.auth_setup(_u_auth)
 
-		_tweets = scr.Scraper()
-		_tweets = _tweets.get_tweets(_auth, _outfile, _num, _hashtag)
+        _tweets = scr.Scraper()
+        _tweets = _tweets.get_tweets(_auth, _outfile, _num, _hashtag)
 
-		print("\n[ANALYSIS] 'Tweet sentiment' data processing")
+        print("\n[ANALYSIS] 'Tweet sentiment' data processing")
 
-		_ptweets = [_tweet for _tweet in _tweets if _tweet['sentiment'] == 'positive']
-		_ntweets = [_tweet for _tweet in _tweets if _tweet['sentiment'] == 'negative']
-		_neutral = len(_tweets) - len(_ntweets) - len(_ptweets)
+        _ptweets = [
+            _tweet for _tweet in _tweets if _tweet['sentiment'] == 'positive']
+        _ntweets = [
+            _tweet for _tweet in _tweets if _tweet['sentiment'] == 'negative']
+        _neutral = len(_tweets) - len(_ntweets) - len(_ptweets)
 
-		print("[POSITIVE | NEGATIVE | NEUTRAL  ]")
-		print("[ {:.2f} %".format(100*len(_ptweets)/len(_tweets)),
-				"|  {:.2f} %".format(100*len(_ntweets)/len(_tweets)),
-				"|  {:.2f} %".format(100*_neutral/len(_tweets)),
-				"]")
+        print("[POSITIVE | NEGATIVE | NEUTRAL  ]")
+        print("[ {:.2f} %".format(100 * len(_ptweets) / len(_tweets)),
+              "|  {:.2f} %".format(100 * len(_ntweets) / len(_tweets)),
+              "|  {:.2f} %".format(100 * _neutral / len(_tweets)),
+              "]")
 
-		_elem = pp.PostProcessor()
+        _elem = pp.PostProcessor()
 
-		_pos_map = _elem.evaluate_text(_parsed_data, str(_hashtag), "positive")
-		_neg_map = _elem.evaluate_text(_parsed_data, str(_hashtag), "negative")
-		_nt_map = _elem.evaluate_text(_parsed_data, str(_hashtag), "neutral")
+        _pos_map = _elem.evaluate_text(_parsed_data, str(_hashtag), "positive")
+        _neg_map = _elem.evaluate_text(_parsed_data, str(_hashtag), "negative")
+        _nt_map = _elem.evaluate_text(_parsed_data, str(_hashtag), "neutral")
 
-		_pos_list = []
-		_neg_list = []
-		_nt_list = []
+        _pos_list = []
+        _neg_list = []
+        _nt_list = []
 
-		for _key in _pos_map:
-			_pos_list.append(_pos_map[_key])
+        for _key in _pos_map:
+            _pos_list.append(_pos_map[_key])
 
-		for _key in _neg_map:
-			_neg_list.append(_neg_map[_key])	
+        for _key in _neg_map:
+            _neg_list.append(_neg_map[_key])
 
-		for _key in _nt_map:
-			_nt_list.append(_nt_map[_key])
+        for _key in _nt_map:
+            _nt_list.append(_nt_map[_key])
 
-		_final_pos = dict()
-		_final_neg = dict()
-		_final_nt = dict()
+        _final_pos = dict()
+        _final_neg = dict()
+        _final_nt = dict()
 
-		for _z in _pos_list:
-			for _e in _z:
-				_final_pos[_e] = int(_final_pos.get(_e, 0) + 1)
+        for _z in _pos_list:
+            for _e in _z:
+                _final_pos[_e] = int(_final_pos.get(_e, 0) + 1)
 
-		for _x in _neg_list:
-			for _y in _x:
-				_final_neg[_y] = int(_final_neg.get(_y, 0) + 1)
+        for _x in _neg_list:
+            for _y in _x:
+                _final_neg[_y] = int(_final_neg.get(_y, 0) + 1)
 
-		for _w in _nt_list:
-			for _t in _w:
-				_final_nt[_t] = int(_final_nt.get(_t, 0) + 1)
+        for _w in _nt_list:
+            for _t in _w:
+                _final_nt[_t] = int(_final_nt.get(_t, 0) + 1)
 
-		_p_file = ((str(_outfile))[:-4]) + "_most_pos.csv"
-		_n_file = ((str(_outfile))[:-4]) + "_most_neg.csv"
-		_nt_file = ((str(_outfile))[:-4]) + "_most_nt.csv"
+        _p_file = ((str(_outfile))[:-4]) + "_most_pos.csv"
+        _n_file = ((str(_outfile))[:-4]) + "_most_neg.csv"
+        _nt_file = ((str(_outfile))[:-4]) + "_most_nt.csv"
 
-		if os.name == 'nt':
-			#Windows
-			with open(_p_file, 'w', newline='') as _csv_file:
-				_writer = csv.writer(_csv_file)
-				for _key, _value in _final_pos.items():
-					if _final_pos[_key] > int(_limit):
-						_writer.writerow([_key, _value])
+        if os.name == 'nt':
+            # Windows
+            with open(_p_file, 'w', newline='') as _csv_file:
+                _writer = csv.writer(_csv_file)
+                for _key, _value in _final_pos.items():
+                    if _final_pos[_key] > int(_limit):
+                        _writer.writerow([_key, _value])
 
-			with open(_n_file, 'w', newline='') as _csv_file:
-				_writer = csv.writer(_csv_file)
-				for _key, _value in _final_neg.items():
-					if _final_neg[_key] > int(_limit):
-						_writer.writerow([_key, _value])
+            with open(_n_file, 'w', newline='') as _csv_file:
+                _writer = csv.writer(_csv_file)
+                for _key, _value in _final_neg.items():
+                    if _final_neg[_key] > int(_limit):
+                        _writer.writerow([_key, _value])
 
-			with open(_nt_file, 'w', newline='') as _csv_file:
-				_writer = csv.writer(_csv_file)
-				for _key, _value in _final_nt.items():
-					if _final_nt[_key] > int(_limit):
-						_writer.writerow([_key, _value])
-		
-		else:
-			#Unix
-			with open(_p_file, 'w') as _csv_file:
-				_writer = csv.writer(_csv_file)
-				for _key, _value in _final_pos.items():
-					if _final_pos[_key] > int(_limit):
-						_writer.writerow([_key, _value])
+            with open(_nt_file, 'w', newline='') as _csv_file:
+                _writer = csv.writer(_csv_file)
+                for _key, _value in _final_nt.items():
+                    if _final_nt[_key] > int(_limit):
+                        _writer.writerow([_key, _value])
 
-			with open(_n_file, 'w') as _csv_file:
-				_writer = csv.writer(_csv_file)
-				for _key, _value in _final_neg.items():
-					if _final_neg[_key] > int(_limit):
-						_writer.writerow([_key, _value])
+        else:
+            # Unix
+            with open(_p_file, 'w') as _csv_file:
+                _writer = csv.writer(_csv_file)
+                for _key, _value in _final_pos.items():
+                    if _final_pos[_key] > int(_limit):
+                        _writer.writerow([_key, _value])
 
-			with open(_nt_file, 'w') as _csv_file:
-				_writer = csv.writer(_csv_file)
-				for _key, _value in _final_nt.items():
-					if _final_nt[_key] > int(_limit):
-						_writer.writerow([_key, _value])
+            with open(_n_file, 'w') as _csv_file:
+                _writer = csv.writer(_csv_file)
+                for _key, _value in _final_neg.items():
+                    if _final_neg[_key] > int(_limit):
+                        _writer.writerow([_key, _value])
 
-	except KeyboardInterrupt:
-		print("[NOTICE] Script interrupted via keyboard (Ctrl+C)")
-		print("Exit...")
-		sys.exit()
+            with open(_nt_file, 'w') as _csv_file:
+                _writer = csv.writer(_csv_file)
+                for _key, _value in _final_nt.items():
+                    if _final_nt[_key] > int(_limit):
+                        _writer.writerow([_key, _value])
+
+    except KeyboardInterrupt:
+        print("[NOTICE] Script interrupted via keyboard (Ctrl+C)")
+        print("Exit...")
+        sys.exit()
