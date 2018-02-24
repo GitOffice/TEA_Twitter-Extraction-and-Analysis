38c03c0b344d3b17df80f737b51fc9630dec0d33
SergioLaRosa <36663933+SergioLaRosa@users.noreply.github.com>
2018-02-24 10:58:14 +0100

    PEP8 fixed

--- common_utils.py
+++ common_utils.py
@@ -1,30 +1,31 @@
 import csv
 import sys
 
+
 class FastWriter():
 
-	def __init__(self):
-		self._status = ""
+    def __init__(self):
+        self._status = ""
 
-	def fast_writer(self, f_name, text_dict, os_name):
-		self._fout = f_name 
-		self._res = text_dict
+    def fast_writer(self, f_name, text_dict, os_name):
+        self._fout = f_name
+        self._res = text_dict
 
-		try:
-			#Windows
-			if os_name == "nt":
-				with open(self._fout, 'w', newline='') as self._csv_file:
-					self._writer = csv.writer(self._csv_file)
-					for self._key, self._value in self._res.items():
-						self._writer.writerow([self._key, self._value])
+        try:
+            # Windows
+            if os_name == "nt":
+                with open(self._fout, 'w', newline='') as self._csv_file:
+                    self._writer = csv.writer(self._csv_file)
+                    for self._key, self._value in self._res.items():
+                        self._writer.writerow([self._key, self._value])
 
-			#Unix/Linux
-			else:
-				with open(self._fout, 'w') as self._csv_file:
-					self._writer = csv.writer(self._csv_file)
-					for self._key, self._value in self._res.items():
-						self._writer.writerow([self._key, self._value])
+            # Unix/Linux
+            else:
+                with open(self._fout, 'w') as self._csv_file:
+                    self._writer = csv.writer(self._csv_file)
+                    for self._key, self._value in self._res.items():
+                        self._writer.writerow([self._key, self._value])
 
-		except:
-			print("[ERROR] Unable to write file on disk. Exit...")
-			sys.exit()
+        except BaseException:
+            print("[ERROR] Unable to write file on disk. Exit...")
+            sys.exit()
